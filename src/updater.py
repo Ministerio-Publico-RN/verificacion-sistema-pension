4765448dbbcf8aaf24ef6fa2e6e6dd1f2392589f
Walter Garay <waltergaraycr@gmail.com>
2026-09-17 10:59:27 -0500

    release: v1.0.1

--- src/updater.py
+++ src/updater.py
@@ -14,7 +14,7 @@ import subprocess
 import threading
 from paths import is_frozen
 
-CURRENT_VERSION = "1.0.0"
+CURRENT_VERSION = "1.0.1"
 GITHUB_REPO = "Ministerio-Publico-RN/verificacion-sistema-pension"
 
 
