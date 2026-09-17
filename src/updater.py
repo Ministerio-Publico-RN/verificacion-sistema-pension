5300d2811a87f466f3e912f75bbc3a0e1df9d709
Walter Garay <waltergaraycr@gmail.com>
2026-09-17 14:15:06 -0500

    release: v1.0.10 con texto actualizado en selector y badge v1.0.10

--- src/updater.py
+++ src/updater.py
@@ -14,7 +14,7 @@ import subprocess
 import threading
 from paths import is_frozen
 
-CURRENT_VERSION = "1.0.9"
+CURRENT_VERSION = "1.0.10"
 GITHUB_REPO = "Ministerio-Publico-RN/verificacion-sistema-pension"
 
 
