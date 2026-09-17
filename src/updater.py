8ac51c762eb0c823b5b2fae004a5b7307c3c26de
Walter Garay <waltergaraycr@gmail.com>
2026-09-17 14:57:37 -0500

    release: v1.0.11 - actualizacion de version y textos de prueba

--- src/updater.py
+++ src/updater.py
@@ -14,7 +14,7 @@ import subprocess
 import threading
 from paths import is_frozen
 
-CURRENT_VERSION = "1.0.10"
+CURRENT_VERSION = "1.0.11"
 GITHUB_REPO = "Ministerio-Publico-RN/verificacion-sistema-pension"
 
 
